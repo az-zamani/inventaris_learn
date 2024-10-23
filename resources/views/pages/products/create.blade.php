2cb5af601ebfbadd83af39e418bc4a23a7c3b968
az-zamani <azacky302@gmail.com>
2024-10-23 12:16:23 +0700

    first commit

---
+++ resources/views/pages/products/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.main')
+
+@section('header')
+<div class="row mb-2">
+    <div class="col-sm-6">
+      <h1>Tambah Produk</h1>
+    </div>
+    <div class="col-sm-6">
+      <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="#">Beranda</a></li>
+        <li class="breadcrumb-item active">Produk</li>
+      </ol>
+    </div>
+  </div>
+@endsection
+
+@section('content')
+  <div class="row">
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+        </div>
+        </div>
+    </div>
+  </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\Admin\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/create', [ProductController::class, 'create']);
